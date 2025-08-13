ed1f65f0bf642898ee1bcb2c43c1c02940dfc751
Holly Jones <166703171+hollyjones12@users.noreply.github.com>
2025-08-13 17:03:46 +0100

    Update Preppin Data 2023 Week 4.sql

--- 2023/Preppin Data 2023 Week 4.sql
+++ 2023/Preppin Data 2023 Week 4.sql
@@ -1,83 +1,65 @@
-with CTE as(
-    select * 
-    ,'PD2023_WK04_JANUARY' as tablename 
-    from PD2023_WK04_JANUARY 
-    Union
-    select * 
-    ,'PD2023_WK04_FEBRUARY' as tablename 
-    from PD2023_WK04_FEBRUARY 
-    Union
-    select * 
-    ,'PD2023_WK04_MARCH' as tablename
-    from PD2023_WK04_MARCH 
-    Union
-    select * 
-    ,'PD2023_WK04_APRIL' as tablename 
-    from PD2023_WK04_APRIL 
-    Union all
-    select * 
-    ,'PD2023_WK04_MAY' as tablename 
-    from PD2023_WK04_MAY 
-    Union all
-    select * 
-    ,'PD2023_WK04_JUNE' as tablename 
-    from PD2023_WK04_JUNE 
-    Union all
-    select *
-    ,'PD2023_WK04_JULY' as tablename 
-    from PD2023_WK04_JULY 
-    Union all
-    select * 
-    ,'PD2023_WK04_AUGUST' as tablename 
-    from PD2023_WK04_AUGUST
-    Union all
-    select *
-    ,'PD2023_WK04_SEPTEMBER' as tablename 
-    from PD2023_WK04_SEPTEMBER
-    Union all
-    select *
-    ,'PD2023_WK04_OCTOBER' as tablename 
-    from PD2023_WK04_OCTOBER 
-    Union all
-    select * 
-    ,'PD2023_WK04_NOVEMBER' as tablename 
-    from PD2023_WK04_NOVEMBER
-    Union all
-    select *
-    ,'PD2023_WK04_DECEMBER' as tablename 
-    from PD2023_WK04_DECEMBER
- )
- 
-,unpivoted_table as (
-select
-id
-,date_from_parts(2023,date_part('month', date(split_part(tablename,'_',3),'MMMM')),joining_day) as joining_date
-,demographic
-,value
-from CTE
-)
+with CTE as (
 
-,pivoted_table as (
-select
-id
-,joining_date
-,ethnicity
-,account_type
-,date_of_birth::date as date_of_birth
-,row_number() over(partition by id order by joining_date ASC) as row_num
-from unpivoted_table
-pivot (Max(value) for demographic in ('Ethnicity', 'Account Type', 'Date of Birth')) as P 
-(id
-,joining_date
-,ethnicity
-,account_type
-,date_of_birth
-))
+    Select *, 'Pd_2023_wk04_january' as table_name from PD2023_WK04_JANUARY
+    UNION
+    Select *, 'Pd_2023_wk04_february' as table_name from PD2023_WK04_FEBRUARY
+    UNION ALL
+    Select *, 'Pd_2023_wk04_march' as table_name from PD2023_WK04_MARCH
+    UNION ALL
+    Select *, 'Pd_2023_wk04_april' as table_name from PD2023_WK04_APRIL
+    UNION ALL
+    Select *, 'Pd_2023_wk04_may' as table_name from PD2023_WK04_MAY
+    UNION ALL
+    Select *, 'Pd_2023_wk04_june' as table_name from PD2023_WK04_JUNE
+    UNION ALL
+    Select *, 'Pd_2023_wk04_july' as table_name from PD2023_WK04_JULY
+    UNION ALL
+    Select *, 'Pd_2023_wk04_august' as table_name from PD2023_WK04_AUGUST
+    UNION ALL
+    Select *, 'Pd_2023_wk04_september' as table_name from PD2023_WK04_SEPTEMBER
+    UNION ALL
+    Select *, 'Pd_2023_wk04_october' as table_name from PD2023_WK04_OCTOBER
+    UNION ALL
+    Select *, 'Pd_2023_wk04_november' as table_name from PD2023_WK04_NOVEMBER
+    UNION ALL
+    Select *, 'Ppd_2023_wk04_december' as table_name from PD2023_WK04_DECEMBER
 
+),
+
+pre_pivot as (
+
+    select id,
+        demographic,
+        value,
+        joining_day,
+        month(date(split_part(table_name,'_',4),'MMMM')) as month,
+        '2023' as year,
+        date_from_parts(year,month,joining_day) as join_date       
+    from CTE
+),
+
+table_pivot as (
+
+    select id,
+           joining_date,
+           ethnicity,
+           account_type, 
+           date_of_birth:: date as date_of_birth,
+           row_number() over(partition by id order by joining_date asc) as rn,
+    from pre_pivot
+    pivot(max(value) for demographic IN ('Ethnicity', 'Account type', 'Date of Birth')) as P
+     (id, 
+      joining_date,
+      ethnicity,
+      account_type,
+      date_of_birth)
+     )
+      
 select
-joining_date
-,account_type
-,date_of_birth
-,ethnicity
-from pivoted_table
-where row_num = 1
+    id,
+    joining_date,
+    account_type,
+    date_of_birth,
+    ethnicity
+from table_pivot
+where rn =1
